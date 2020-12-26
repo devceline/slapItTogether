c438c4bf6d85f99b1a3e9f0e0b1e0c057ea87a42
Shaheen Sarafa <shaheensarafa17@gmail.com>
2020-12-26 11:57:14 +0200

    Setting up glew & glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
-
+#include <iostream>
+#include <GL/glew.h>   
+#include "GLFW/glfw3.h"
 
 int main(void){
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Testing OpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK){
+        std::cout << "GLEW FAIL" << std::endl;
+        return 123;
+    }
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        if(GLFW_KEY_Q == GLFW_KEY_DOWN){
+            glfwSetWindowShouldClose(window, 1);
+        }
+        glfwSwapBuffers(window);
+    }
+
+
+
     return 0;
 }
